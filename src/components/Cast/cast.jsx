672c0d27a478f87672f9fff1f918d565286c8255
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2022-04-18 00:36:36 +0300

    upd

--- src/components/Cast/cast.jsx
+++ src/components/Cast/cast.jsx
@@ -1,3 +1,32 @@
-export default function Cast() {
-    return <></>
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import API from 'fetch/fetch';
+import s from './cast.module.css'
+
+
+export default function Cast (){
+    const { moviesId } = useParams();
+    const [cast, setCast] = useState();
+
+    useEffect(() => {
+        API.fetchMovieCast(moviesId).then(data => setCast(data.cast))
+    }, [moviesId])
+
+    return (
+        <>
+        {cast && (
+                <ul>
+                {cast.map(actor => (
+                    <li key={actor.id} className={s.item}> 
+                        <img className={s.img} src={`https://image.tmdb.org/t/p/w342${actor.profile_path}`} alt={actor.name} />
+                        <h3>{actor.name}</h3>
+                        <h4>Character: {actor.character}</h4>
+                    </li>
+                ))}
+            </ul>
+        )}
+        
+        </>
+
+    )
 }
