ac333c498c83130746c098817f87bbcfad0c98c6
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2022-04-18 01:20:18 +0300

    Update button.jsx

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,3 +1,20 @@
-export default function Button() {
-    return <></>
+import PropTypes from 'prop-types';
+
+export default function Button({type, onClick, children}){
+    return (
+        <button type={type} onClick={onClick}>
+            {children}
+        </button>
+    )
 }
+
+Button.defaultProps = {
+    onClick: () => null,
+    children: null,
+};
+  
+Button.propTypes = {
+    type: PropTypes.oneOf(['button', 'submit', 'reset']).isRequired,
+    onClick: PropTypes.func,
+    children: PropTypes.node,
+};
