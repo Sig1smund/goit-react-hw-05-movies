6b06be1edb202670c26b7dde6007bed9c58c3393
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2022-04-19 23:19:53 +0300

    FIX

--- src/components/ListMovies/listMovies.jsx
+++ src/components/ListMovies/listMovies.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import API from 'fetch/fetch';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Spinner from 'components/Spinner/spinner';
 
 export default function ListMovies({search}) {
-    const [movies, setMovies] = useState()
-    const [spinner, setSpinner] = useState(false)
-    const location = useLocation()
+    const [movies, setMovies] = useState();
+    const [spinner, setSpinner] = useState(false);
+    const location = useLocation();
+
     useEffect(() => {
-        if(!search){
+        if (!search) {
             return;
         }
         setSpinner(true);
@@ -16,24 +19,33 @@ export default function ListMovies({search}) {
             .then(data => {
                 console.log(data)
                 if (data.results.length === 0) {
-                    window.alert('Русский корабль, иди на#уй')
+                    toast.error('Русский корабль, иди на#уй', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
                 }
                 setMovies(data.results)
             }).finally(() => { setSpinner(false) })
-    }, [search])
+    }, [search]);
 
     return (
         <>
-        {spinner && <Spinner/>}
-        {movies && (
-            <ul>
-                {movies.map(movie => (
-                    <li key={movie.id}>
-                         <Link to={`${movie.id}`} state={{ from: location, label: 'Go to Search' }}>{movie.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        )}
+            {spinner && <Spinner />}
+            {movies && (
+                <ul>
+                    {movies.map(movie => (
+                        <li key={movie.id}>
+                            <Link to={`${movie.id}`} state={{ from: location, label: 'Go to Search' }}>{movie.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <ToastContainer />
         </>
-    )
+    );
 }
