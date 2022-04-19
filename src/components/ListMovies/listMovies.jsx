ec29a5d879591d3a28bebc599bc95871daf39eae
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2022-04-19 22:15:18 +0300

    Update listMovies.jsx

--- src/components/ListMovies/listMovies.jsx
+++ src/components/ListMovies/listMovies.jsx
@@ -1,21 +1,30 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import API from 'fetch/fetch';
+import Spinner from 'components/Spinner/spinner';
 
 export default function ListMovies({search}) {
     const [movies, setMovies] = useState()
+    const [spinner, setSpinner] = useState(false)
     const location = useLocation()
     useEffect(() => {
         if(!search){
             return;
         }
-
+        setSpinner(true);
         API.fetchSearchMovie(search)
-            .then(data => setMovies(data.results))
+            .then(data => {
+                console.log(data)
+                if (data.results.length === 0) {
+                    window.alert('Русский корабль, иди на#уй')
+                }
+                setMovies(data.results)
+            }).finally(() => { setSpinner(false) })
     }, [search])
 
     return (
         <>
+        {spinner && <Spinner/>}
         {movies && (
             <ul>
                 {movies.map(movie => (
