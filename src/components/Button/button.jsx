1114c31e2f06733d61b00a9d420d9a227c82b160
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2022-04-20 00:12:10 +0300

    upd

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -2,19 +2,13 @@ import PropTypes from 'prop-types';
 import s from './button.module.css'
 export default function Button({type, onClick, children}){
     return (
-        <button className={s.button}type={type} onClick={onClick}>
+        <button className={s.button} type={type} onClick={onClick}>
             {children}
         </button>
     )
 }
-
-Button.defaultProps = {
-    onClick: () => null,
-    children: null,
-};
   
 Button.propTypes = {
-    type: PropTypes.oneOf(['button', 'submit', 'reset']).isRequired,
     onClick: PropTypes.func,
     children: PropTypes.node,
 };
--- src/components/ListMovies/listMovies.jsx
+++ src/components/ListMovies/listMovies.jsx
@@ -21,7 +21,7 @@ export default function ListMovies({search}) {
                 if (data.results.length === 0) {
                     toast.error('Русский корабль, иди на#уй', {
                         position: "top-center",
-                        autoClose: 5000,
+                        autoClose: 3000,
                         hideProgressBar: false,
                         closeOnClick: true,
                         pauseOnHover: true,
