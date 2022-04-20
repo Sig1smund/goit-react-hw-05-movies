a2f636af33c86a1ca7a2ec569553ba873a6456cc
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2022-04-20 13:55:32 +0300

    Update searchMovie.jsx

--- src/components/SearchMovie/searchMovie.jsx
+++ src/components/SearchMovie/searchMovie.jsx
@@ -1,25 +1,36 @@
 import { useState } from 'react';
 import Button from 'components/Button/button';
 import s from './searchMovie.module.css'
+import { toast } from 'react-toastify';
 
 export default function SearchMovie({forSearch, search}) {
-        const [input, setInput] = useState('');
+    const [input, setInput] = useState('');
+    
+    const handleFromSubmit = e => {
+        e.preventDefault();
+
+        if (!input) {
+            toast.warn('No keyword given to initialize search!', {position: 'top-center'});
+            forSearch('');
+            e.currentTarget.reset();
+            return;
+        }
+        
+        forSearch(input);
+        e.currentTarget.reset();
+    }
 
     return (
-        <form className={s.search__form} onSubmit={e => {
-            e.preventDefault();
-            if(!input){
-                return;
-            }
-            forSearch(input);
-          }}>
+        <>
+            <form className={s.search__form} onSubmit={handleFromSubmit}>
             <input type="text" className={s.search__input}
-                placeholder={search ?? 'Search for a movie...'}
+                placeholder={'Search for a movie...'}
                 onChange={e => setInput(e.target.value.trim())}
             />
             <Button type="submit">
                 Search
             </Button>
-        </form>
+            </form>
+        </>
     )
 }
